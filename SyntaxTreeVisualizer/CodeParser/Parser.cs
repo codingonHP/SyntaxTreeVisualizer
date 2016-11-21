80133228ecc0d23b3df88b2cbfab94ae6f0119f3
Vishal Anand <vishal_24_anand@hotmail.com>
2016-11-21 14:37:37 +0530

    Colored the tree nodes based on SyntaxNode, Token and Trivia.
    Added Trivia nodes to the Tree

--- SyntaxTreeVisualizer/CodeParser/Parser.cs
+++ SyntaxTreeVisualizer/CodeParser/Parser.cs
@@ -1,16 +1,12 @@
 ﻿using System.Windows.Controls;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using SyntaxTreeVisualizer.Colors;
 
 namespace SyntaxTreeVisualizer.CodeParser
 {
     public class Parser
     {
-        public CSharpSyntaxTree CreateSyntaxTree(string sourceCode)
-        {
-            return (CSharpSyntaxTree)CSharpSyntaxTree.ParseText(sourceCode);
-        }
-
         public string FormatSourceCode(string sourceCode)
         {
             var syntaxTree = CreateSyntaxTree(sourceCode);
@@ -27,20 +23,56 @@ namespace SyntaxTreeVisualizer.CodeParser
             TraverseCodeDom(compilationUnit, sourceCodeTree.Items[0] as TreeViewItem);
         }
 
+        private CSharpSyntaxTree CreateSyntaxTree(string sourceCode)
+        {
+            return (CSharpSyntaxTree)CSharpSyntaxTree.ParseText(sourceCode);
+        }
+
         private void TraverseCodeDom(SyntaxNode currentRoot, TreeViewItem node)
         {
             var decendents = currentRoot.ChildNodesAndTokens();
-
             foreach (var syntaxNode in decendents)
             {
-                var headerNode = new TreeViewItem { Header = $"{syntaxNode.Kind()}" };
+                var headerNode = new TreeViewItem
+                {
+                    Header = $"{syntaxNode.Kind()}",
+                    Foreground = TreeNodeColor.ColorNode(syntaxNode)
+                };
+
                 node.Items.Add(headerNode);
+                AddTriviaIfAvailable(syntaxNode, node);
 
                 if (syntaxNode.IsNode)
                 {
                     TraverseCodeDom((SyntaxNode)syntaxNode, node.Items[node.Items.Count - 1] as TreeViewItem);
                 }
             }
+        }
+
+        private void AddTriviaIfAvailable(SyntaxNodeOrToken node, TreeViewItem treeNodeItem)
+        {
+            if (node.HasLeadingTrivia)
+            {
+                var trivia = node.GetLeadingTrivia()[0];
+                WriteTriviaNode(treeNodeItem, trivia, "Leading Trivia - ");
+            }
+
+            if (node.HasTrailingTrivia)
+            {
+                var trivia = node.GetTrailingTrivia()[0];
+                WriteTriviaNode(treeNodeItem, trivia, "Trailing Trivia - ");
+            }
+        }
+
+        private void WriteTriviaNode(TreeViewItem treeNodeItem, SyntaxTrivia syntaxTrivia, string message = "")
+        {
+            var lastNode = treeNodeItem.Items[treeNodeItem.Items.Count - 1] as TreeViewItem;
+
+            lastNode?.Items.Add(new TreeViewItem
+            {
+                Header = $"{message} { syntaxTrivia.Kind() } - {syntaxTrivia.FullSpan}",
+                Foreground = TreeNodeColor.ColorNode(null)
+            });
         }
     }
 }
---
+++ SyntaxTreeVisualizer/Colors/TokenColor.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Media;
+using Microsoft.CodeAnalysis;
+
+namespace SyntaxTreeVisualizer.Colors
+{
+    public static class TreeNodeColor
+    {
+        public static SolidColorBrush ColorNode(SyntaxNodeOrToken node)
+        {
+            if (node == null)
+            {
+                return new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            }
+
+            if (node.IsNode)
+            {
+                return new SolidColorBrush(Color.FromRgb(0, 0, 255));
+            }
+
+            if (node.IsToken)
+            {
+                return new SolidColorBrush(Color.FromRgb(29, 101, 7));
+            }
+
+            return new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+    }
+}
